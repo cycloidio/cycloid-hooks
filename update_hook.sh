3abee3895467d2cc3f842d0535de9988e4ddd8b7
Hugo Rosnet <hugo.rosnet@cycloid.io>
2016-05-26 15:37:34 +0200

    feat: Adding verbose option to the script
    Verbose doesn't add much output, but it makes it easier to follow what
    is going on in the run and also helps to debug if need be.
    
    Ideally options should be added for the different variables.

--- update_hook.sh
+++ update_hook.sh
@@ -3,6 +3,7 @@
 # Modify those variables with your path
 GIT_DIR="$HOME/git/work/"
 TEMPLATE_HOOK_DIR="$HOME/.git_template/hooks/"
+VERBOSE=0
 
 FIND_BIN=$(which find)
 RM_BIN=$(which rm)
@@ -15,12 +16,16 @@ PATHS=$(${FIND_BIN} ${GIT_DIR} -maxdepth 2 -iname '.git' -type d)
 
 for PATH in $(echo ${PATHS})
 do
+	[ ${VERBOSE} ] && echo "Updating $(${BASENAME_BIN} $(${DIRNAME_BIN} ${PATH}))..."
 	for HOOK in $(echo "${HOOKS}")
 	do
 		if [ -f "${PATH}/hooks/${HOOK}" ]; then
+			[ ${VERBOSE} ] && echo -e "\tFound ${HOOK}, deleting it"
 			${RM_BIN} "${PATH}/hooks/${HOOK}"
 		fi
 	done
+	[ ${VERBOSE} ] && echo -e "\tUsing latest template $(${DIRNAME_BIN} ${PATH})"
 	${GIT_BIN} init $(${DIRNAME_BIN} ${PATH}) > /dev/null
+	[ ${VERBOSE} ] && echo "Done!"
 done
 
