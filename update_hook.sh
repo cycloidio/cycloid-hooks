ddd9f48e4490349184e5bfa448eaf16b58844b62
Hugo Rosnet <hugo.rosnet@cycloid.io>
2016-05-26 17:01:28 +0200

    feat: Updated update_hooks.sh to take parameters and display usage
    The update_hooks script should be at a good state now, and it also
    includes parameters which allow to easily control and adapt people's
    needs.
    
    Usage and example have been added to make sure everybody understand how
    to use it.

--- update_hook.sh
+++ update_hook.sh
@@ -1,9 +1,63 @@
 #!/bin/bash
+#
+# This script updates all hooks located in the TEMPLATE_HOOK_DIR of all repositories located under the GIT_DIR.
+# This allows to easily update hooks to use their latest version.
+#
 
-# Modify those variables with your path
-GIT_DIR="$HOME/git/work/"
-TEMPLATE_HOOK_DIR="$HOME/.git_template/hooks/"
+usage()
+{
+cat << EOF
+usage: $0 -r GIT_DIR -t TEMPLATE_HOOK_DIR [-v]
+
+This script updates all hooks located in the TEMPLATE_HOOK_DIR of all repositories located under the GIT_DIR.
+This allows to easily update hooks to use their latest version.
+
+OPTIONS:
+   -r       Directory containing all repositories
+   -t       Template directory containing hooks
+   -v       Verbose enablement
+
+Example:
+   $0 -r $HOME/git/work/ -t $HOME/.git_template/hooks/ -v
+EOF
+}
+
+GIT_DIR=""
+TEMPLATE_HOOK_DIR=""
 VERBOSE=0
+
+while getopts "vt:r:h" opt; do
+  case $opt in
+    r)
+      GIT_DIR=$OPTARG
+      ;;
+    t)
+      TEMPLATE_HOOK_DIR=$OPTARG
+      ;;
+    v)
+      VERBOSE=1
+      ;;
+    ?)
+      usage
+      exit 0
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+  esac
+done
+
+if [ ! -d "${GIT_DIR}" ]; then
+	echo "Error: Git root directory requires an argument."
+	usage
+	exit 1
+fi
+if [ ! -d "${TEMPLATE_HOOK_DIR}" ]; then
+	echo "Error: Template directory requires an argument."
+	usage
+	exit 1
+fi
 
 FIND_BIN=$(which find)
 RM_BIN=$(which rm)
